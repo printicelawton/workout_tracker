f24e86a3aaf062dae7c050f1500d2d117ede64df
Printice Lawton <printicelaw10@gmail.com>
2021-07-12 11:56:17 -0400

    server up and running

--- server.js
+++ server.js
@@ -12,8 +12,14 @@ app.use(express.urlencoded ({ extended: true }));
 app.use(express.json());
 
 // make the mongoose connection 
-mongoose.connect(process.env.MONGODB_URI || "mongod://localhost/workout", {
-    useNewUrlParser: true, 
-    useFindAndModify: false
+// mongoose.connect(process.env.MONGODB_URI || "mongod://localhost/workout", {
+//     useNewUrlParser: true, 
+//     useFindAndModify: false
+// });
+
+//  api routes to 
+
+app.listen(PORT, () =>{
+    console.log(`App is listening on port ${PORT}!`);
 });
 
