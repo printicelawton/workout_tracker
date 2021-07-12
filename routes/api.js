dfbfd843556842682f3fb5d8014f379ea4a560f4
Printice Lawton <printicelaw10@gmail.com>
2021-07-12 17:39:24 -0400

    working on routes

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,22 @@
-// const express = require ("express");
 const router = express.Router(); 
-const db = require("../model/workout.js");
+const Workout = require("../model/workout.js");
 
-router.post("/api/workouts", (req, res) => {
+router.post("/api/workouts", (res, req) => {
     Workout.create({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-});
-
-router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-      .then((dbWorkout) => res.json(dbWorkout))
-      .catch((err) => {
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
         res.json(err);
-      });
-  });
+    });
+});
 
 router.put("/api/workout/:id", ({ body, params }, res) => {
     Workout.findByIdAndUpdate (
         params.id, 
         { $push: {exercises: body}},
         // makes sure new exercises fits schema requirements
+        { new: true, runValidators: true }
     )
     .then(dbWorkout => {
         res.json(dbWorkout);
@@ -37,9 +29,32 @@ router.put("/api/workout/:id", ({ body, params }, res) => {
 router.get("/api/workouts", (req, res) => {
     Workout.find()
     .then(dbWorkout => {
-        res.json(dbWorkout)
-    });
-})
-.catch(err => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
     res.json(err);
-})
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+      .then(dbWorkouts => {
+        console.log(dbWorkouts)
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+//   router.delete("/api/workouts", ({ body }, res) => {
+//     Workout.findByIdAndDelete(body.id)
+//       .then(() => {
+//         res.json(true);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+  
+  module.exports = router;
