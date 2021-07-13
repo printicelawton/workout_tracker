6731e23d9cc91305ce307ea117b56eec0e715baa
Printice Lawton <printicelaw10@gmail.com>
2021-07-13 12:24:48 -0400

    last time working with routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(); 
 const Workout = require('../models/workout.js');
-// const db = require ('./models');
+// const db = require ('../models');
 
 router.post('/api/workouts', (req,res) => {
     Workout.create({})
